0270ca958984165db88f5f9014628504554a2a57
AlperenOz <alperenoz6001@gmail.com>
2020-08-16 15:56:22 -0400

    yeni class ilk mesaj

--- src/git_project_sonders/SonDersTekrar.java
+++ src/git_project_sonders/SonDersTekrar.java
@@ -3,7 +3,7 @@ package git_project_sonders;
 public class SonDersTekrar {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("3:55 master branch");
 
 	}
 
